2b0f9522073872961d654b73db7fd68f91584912
hondajohn88 <hondajohn88@gmail.com>
2017-08-11 16:04:07 -0700

    Replace Eleven with Vanilla.

--- msm8996.mk
+++ msm8996.mk
@@ -216,6 +216,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Music
+PRODUCT_PACKAGES += \
+    VanillaMusic
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
